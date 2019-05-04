67f897ba311791676eabd1e0a1da0ebc0fc899b6
lazaruslin <635613333@qq.com>
2019-05-05 03:48:18 +0800

    bishe3

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const sensor = require("node-dht-sensor");
 const app = express();
 app.get("/temperature",(req, res) => {
-	res.send("24°C");
+    sensor.read(11, 17, function(err, temperature, humidity) {
+		if(!err) {
+			let dht_temperature = temperature.toFixed(1);
+			console.log(dht_temperature);
+			res.send(`${dht_temperature}°C`);
+		}
+	});
 });
 app.get("/humidity",(req, res) => {
-	res.send("48%")
+    sensor.read(11, 17, function(err, temperature, humidity) {
+		if(!err) {
+			let dht_humidity = humidity.toFixed(1);
+			console.log(dht_humidity);
+			res.send(`${dht_humidity}%`);
+		}
+	});
 });
 app.listen(3000, () => {
 	console.log("Server listening on port 3000")
