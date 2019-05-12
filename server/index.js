6031def550f73b070ce6c62a3e7ad362ee0f782e
lazaruslin <635613333@qq.com>
2019-05-12 19:20:23 +0800

    bishe3

--- server/index.js
+++ server/index.js
@@ -1,18 +1,29 @@
 const express = require("express");
 const get_cached_readings = require("./get_cached_readings");
+const light_control=require("./light-control");
 const path=require("path");
 const app = express();
 app.use("/public",express.static(path.join(__dirname,"public")));
 app.get("/temperature", function(req, res){
-    let my_temperature = get_cached_readings.get_temperature();
-    console.log(my_temperature);
+	let my_temperature = get_cached_readings.get_temperature();
+	console.log(my_temperature);
 	res.json({val:my_temperature});
 });
 app.get("/humidity", function(req, res){
-    let my_humidity = get_cached_readings.get_humidity();
+	let my_humidity = get_cached_readings.get_humidity();
 	console.log(my_humidity);
 	res.json({val:my_humidity});
 });
+app.get("/on", function(req, res){
+	light_control.on();
+	res.send("light is on")
+	console.log("light is on");
+});
+app.get("/off", function(req, res){
+	light_control.off();
+	res.send("light is off");
+	console.log("light is off");
+});
 app.listen(3000, () => {
-	console.log("Server listening on port 3000")
+	console.log("Server listening on port 3000");
 });
--- server/public/script.js
+++ server/public/script.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
-	
+	//定义一个更新图表数组的函数,接收一个数组作为参数，和一个固定的长度
 	const update_chart_array=(arr,val,size)=>{
-		
+		/*使用数组的push和shift方法来得到一个固定长度的数组*/
 		arr.push(val);
 		if(arr.length>size)
 		{
 			arr.shift()
 		}
 	}
-	
-	
+	//chart.js 图表
+	//温度图表
 	const temperatureCanvasCtx = $("#temperature_chart")[0].getContext("2d");
 	const temperatureChart_options={
-		
+		//将温度信息显示为现状图
 		type: "line",
 		data: {
-			labels: [],
+			labels: [],//保存时间点
 			datasets: [{
-				data: [],
+				data: [],//保存传感器值
 				backgroundColor: "rgba(0,200,100,0.5)"
 			}]
 		},
@@ -31,16 +31,16 @@ $(document).ready(function() {
 		}
 	}
 	const temperatureChart = new Chart(temperatureCanvasCtx,temperatureChart_options);
-	
+	//湿度图表
 	const humidityCanvasCtx = $("#humidity_chart")[0].getContext("2d");
 	const humidityChart_options={
-		
+		//将温度信息显示为现状图
 		type: "line",
 		data: {
-			labels: [],
+			labels: [],//保存时间点
 			datasets: [{
-				data: [],
-				backgroundColor: "rgba(0,200,100,0.5)"
+				data: [],//保存传感器值
+				backgroundColor: "rgba(200,0,100,0.5)"
 			}]
 		},
 		options:{
@@ -53,16 +53,16 @@ $(document).ready(function() {
 		}
 	}
 	const humidityChart = new Chart(humidityCanvasCtx,humidityChart_options);
-	
+	//fetch api
 	const fetch_temperature = () => {
 		fetch('/temperature').then(response => response.json())
-		    .then(data => {
-				
+			.then(data => {
+				//获取当前时间，更新图表中的labels数组
 				const now =new Date();
 				const timeNow=`${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
 				update_chart_array(temperatureChart_options.data.labels,timeNow,10);
 				update_chart_array(temperatureChart_options.data.datasets[0].data,data.val,10);
-				temperatureChart.update();
+				temperatureChart.update();//动态更新图表
 				console.log(data.val);
 				$("#temperature").text(data.val);
 			})
@@ -70,21 +70,38 @@ $(document).ready(function() {
 	};
 	const fetch_humidity = () => {
 		fetch('/humidity').then(response => response.json())
-		    .then(data => {
-				
+			.then(data => {
+				//获取当前时间，更新图表中的labels数组
 				const now =new Date();
 				const timeNow=`${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
 				update_chart_array(humidityChart_options.data.labels,timeNow,10);
 				update_chart_array(humidityChart_options.data.datasets[0].data,data.val,10);
-				humidityChart.update();
+				humidityChart.update();//动态更新图表
 				console.log(data.val);
 				$("#humidity").text(data.val);
-			}
+			})
 			.catch(e => console.log("Oops, error", e))
 	}
-	
+	$("#light_on").click(() => {
+		fetch('/on').then(response => response.text())
+			.then(data => {
+				$("#light_info").text(data);
+				console.log(data);
+			})
+			.catch(e => console.log("Oops, error", e))
+	})
+	$("#light_off").click(() => {
+		fetch('/off').then(response => response.text())
+			.then(data => {
+				$("#light_info").text(data);
+				console.log(data);
+			})
+			.catch(e => console.log("Oops, error", e))
+	})
+
 	setInterval(() => {
 		fetch_humidity();
 		fetch_temperature();
-	},2000)
+	}, 2000)
 	
+});
