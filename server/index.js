efc0b008b703da349e9d1b6d47bac2a7d00aa92a
lazaruslin <635613333@qq.com>
2019-05-12 19:09:34 +0800

    bishe3

--- server/index.js
+++ server/index.js
@@ -6,12 +6,12 @@ app.use("/public",express.static(path.join(__dirname,"public")));
 app.get("/temperature", function(req, res){
     let my_temperature = get_cached_readings.get_temperature();
     console.log(my_temperature);
-	res.json({val:temperature});
+	res.json({val:my_temperature});
 });
 app.get("/humidity", function(req, res){
     let my_humidity = get_cached_readings.get_humidity();
 	console.log(my_humidity);
-	res.json({val:_humidity});
+	res.json({val:my_humidity});
 });
 app.listen(3000, () => {
 	console.log("Server listening on port 3000")
