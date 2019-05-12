91e9fc961e16c8a2ac551a9822e4b842468de6c6
lazaruslin <635613333@qq.com>
2019-05-12 18:02:21 +0800

    bishe

--- server/public/script.js
+++ server/public/script.js
@@ -20,4 +20,22 @@ $(document).ready(function() {
 		fetch_humidity();
 		fetch_temperature();
 	},2000)
+	
+	const temperatureCanvasCtx = $("#temperature_chart")[0].getContext("2d");
+	const temperatyreCanvasCtx = new Chart(temperatureCanvasCtx, {
+		
+		type: "line",
+		data: {
+			labels: ["8:10","8:11","8:12","8:13","8:14","8:15","8:16","8:17","8:18","8:19"],
+			dataset: [{
+				data: [13, 12, 10, 17, 20, 18, 14, 16, 11, 13],
+				backgroundColor: "rgba(0,200,100,0.5)"
+			}]
+		},
+		options:{
+			legend:{display:false},
+			responsive:true,
+			maintainAspectRatio:true
+		}
+	});
 });
